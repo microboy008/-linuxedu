89c1788faeeabb31ab0329b4443f37f1fb410516
microboy008 <okboy008@gmail.com>
2018-09-01 09:54:05 +0800

    10.sh del

--- 10.sh
+++ 10.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-slfkjslakj
-=======
 #test
->>>>>>> 4d5d0439f5ab630b09eaac2f84f49fd34dd50f90
 password=""
 origin="zxcvbnmasdfghjklqwertyuiopQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
 for i in `seq $1`
