90a3bb5063f69bdafbf20091e61243adec91016f
microboy008 <okboy008@gmail.com>
2018-11-05 18:58:47 +0800

    add

--- lftp-sync.sh
+++ lftp-sync.sh
@@ -2,3 +2,7 @@
 lftp -u Nsd1806,Nsd1806 -e "mirror --only-newer --verbose / /root/note/" 176.204.15.117 <<EOF
 bye
 EOF
+lftp  -e "mirror --only-newer --verbose / /root/note/" 176.204.15.117 <<EOF
+bye
+EOF
+
